363c9cb9faa5d18e4f4102a343ff75c62e83293c
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-12-29 14:34:24 +0530

    Inject deployed contract's address and ABI to create local abstraction of the deployed contract.

--- src/web3.js
+++ src/web3.js
@@ -2,4 +2,8 @@ import Web3 from "web3";
 
 const web3 = new Web3(window.web3.currentProvider);
 
-export default web3;
+const address = "0x83321c9770178CBa6F771ab5279ea75B89eA3994";
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
